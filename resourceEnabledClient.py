5e82663192a6206dfb237e0fa35446f6d6c01e5f
fastfingertips <fastfingertips@gmail.com>
2022-02-12 16:40:52 +0300

    Update resourceEnabledClient.py

--- resourceEnabledClient.py
+++ resourceEnabledClient.py
@@ -1,4 +1,3 @@
-# pip install requests
 import requests
 import datetime
 from urllib.parse import urlencode
@@ -13,10 +12,10 @@ class SpotifyAPI(object):
     access_token_did_expire = True
     client_id = None
     client_secret = None
-    token_url = 'https://accounts.spotify.com/api/token'
-
+    token_url = "https://accounts.spotify.com/api/token"
+    
     def __init__(self, client_id, client_secret, *args, **kwargs):
-        super().__init__(*args,**kwargs) 
+        super().__init__(*args, **kwargs)
         self.client_id = client_id
         self.client_secret = client_secret
 
@@ -27,24 +26,24 @@ class SpotifyAPI(object):
         client_id = self.client_id
         client_secret = self.client_secret
         if client_secret == None or client_id == None:
-            raise Exception("You must set client_id and client secret")
-        client_creds = f'{client_id}:{client_secret}'
+            raise Exception("You must set client_id and client_secret")
+        client_creds = f"{client_id}:{client_secret}"
         client_creds_b64 = base64.b64encode(client_creds.encode())
         return client_creds_b64.decode()
-
+    
     def get_token_headers(self):
         client_creds_b64 = self.get_client_credentials()
         return {"Authorization": f"Basic {client_creds_b64}"}
-
+    
     def get_token_data(self):
-        return {"grant_type":"client_credentials"}
-
+        return {"grant_type": "client_credentials"} 
+    
     def perform_auth(self):
         token_url = self.token_url
         token_data = self.get_token_data()
         token_headers = self.get_token_headers()
         r = requests.post(token_url, data=token_data, headers=token_headers)
-        if r.status_code not in range(200,299):
+        if r.status_code not in range(200, 299):
             raise Exception("Could not authenticate client.")
             # return False
         data = r.json()
@@ -56,7 +55,7 @@ class SpotifyAPI(object):
         self.access_token_expires = expires
         self.access_token_did_expire = expires < now
         return True
-
+    
     def get_access_token(self):
         token = self.access_token
         expires = self.access_token_expires
@@ -66,14 +65,14 @@ class SpotifyAPI(object):
             return self.get_access_token()
         elif token == None:
             self.perform_auth()
-            return self.get_access_token()
+            return self.get_access_token() 
         return token
-
+    
     def get_resource_header(self):
         access_token = self.get_access_token()
         headers = {"Authorization": f"Bearer {access_token}"}
         return headers
-
+        
     def get_resource(self, lookup_id, resource_type='albums', version='v1'):
         endpoint = f"https://api.spotify.com/{version}/{resource_type}/{lookup_id}"
         headers = self.get_resource_header()
@@ -81,22 +80,24 @@ class SpotifyAPI(object):
         if r.status_code not in range(200, 299):
             return {}
         return r.json()
-
+    
     def get_album(self, _id):
         return self.get_resource(_id, resource_type='albums')
-
+    
     def get_artist(self, _id):
         return self.get_resource(_id, resource_type='artists')
-
-    def search(self, query, search_type='artist'): # type
+    
+    def search(self, query, search_type='artist' ): # type
         headers = self.get_resource_header()
         endpoint = "https://api.spotify.com/v1/search"
         data = urlencode({"q": query, "type": search_type.lower()})
         lookup_url = f"{endpoint}?{data}"
         r = requests.get(lookup_url, headers=headers)
-        if r.status_code not in range(200, 299):
+        if r.status_code not in range(200, 299):  
             return {}
         return r.json()
 
 spotify = SpotifyAPI(client_id, client_secret)
-print(spotify.search("Ezhel Geceler", search_type="track"))
+spotify.search("Ezhel Geceler", search_type="track")
+spotify.get_artist("6LnJKrtFnTEGdbWQ2riWCL") # Ezhel
+spotify.get_album("5nEbYpL2jivoHAmYEG5bbD") # Hayrola
